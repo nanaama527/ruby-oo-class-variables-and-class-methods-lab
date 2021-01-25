40d44c25f38d5339d5293c59a80ab17422676984
Nana <72888369+nanaama527@users.noreply.github.com>
2021-01-24 22:24:28 -0500

    Update song.rb

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song
+    
+    attr_accessor :name, :artist, :genre
+    
+    @@artists = []
+    @@genres = []
+    @@count = 0
+    
+    def initialize(name, artist, genre)
+      @@count += 1
+      @name = name
+      @artist = artist
+      @genre = genre
+      @@artists << @artist
+      @@genres << @genre
+    end
+    
+    def self.count
+      @@count
+    end
+    
+    def self.artists
+        @@artist
+    end
+  end
